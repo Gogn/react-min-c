ce5f8cd96338bc4c8fce79655a250ac4927a2141
Dero <106393@gmail.com>
2020-01-02 22:37:46 +0400

    Updates

---
+++ precache-manifest.0af79b5236590bcb08619d3164deb3aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f65a03a7939027c6723088ffb6226e",
+    "url": "/react-min-c/index.html"
+  },
+  {
+    "revision": "839465f197aa56257ba3",
+    "url": "/react-min-c/static/css/main.0ce4ac7f.chunk.css"
+  },
+  {
+    "revision": "28e4049fb0d28da34209",
+    "url": "/react-min-c/static/js/2.57087b5d.chunk.js"
+  },
+  {
+    "revision": "3159dcd1f0681bdd4921a3bb6680c3d5",
+    "url": "/react-min-c/static/js/2.57087b5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "839465f197aa56257ba3",
+    "url": "/react-min-c/static/js/main.4d67e992.chunk.js"
+  },
+  {
+    "revision": "68c8480cc37d46083fba",
+    "url": "/react-min-c/static/js/runtime-main.49c0b96d.js"
+  }
+]);
